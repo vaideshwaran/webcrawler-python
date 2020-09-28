a095d8e194b0ec6cf8b73a7077327df11a9a11ec
vaideshwaran <34024920+vaideshwaran@users.noreply.github.com>
2020-09-28 13:46:42 +0530

    updated download_images.py

--- download_images.py
+++ download_images.py
@@ -7,21 +7,32 @@ import re
 import requests
 
 i=1
+
+#fetches the url & parse the html content using beautifulsoap
 def fetch(url):
     thepage= urllib.request.urlopen(url)
     soupdata = BeautifulSoup(thepage, "html.parser")
     return soupdata
 
 soup = fetch("https://pixels.com/")
+
+#fetches all the href links in the <a> tag & prints the same
+for link in soup.findAll('a', attrs={'href': re.compile("^https://")}):
+    href_links = link.get('href')
+    print(href_links)
+    
+#fetches all the src content in the <img> tag of the page & prints the same    
 for img in soup.findAll('img', attrs={'src': re.compile("^https://")}):
     temp=img.get('src')
+    
     if temp[:1]=="/":
         image = "https://pixels.com/" + temp
     else:
         image = temp
-
+        
     print(image)
 
+#Naming the files which are to be donwloaded
     nametemp = img.get('alt')
     if len(nametemp)==0:
         filename=str(i)
@@ -29,7 +40,7 @@ for img in soup.findAll('img', attrs={'src': re.compile("^https://")}):
     else:
         filename=nametemp
 
-
+ #Writing the files & downloading images
     imagefile = open(filename + ".jpeg", 'wb')
     imagefile.write(urllib.request.urlopen(image).read())
     imagefile.close()
